902582a285ba569665aff63d03da5ddc316046b4
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-05-11 15:03:32 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -22,11 +22,17 @@ async def render_page(id, secure_hash):
             heading = 'Watch {}'.format(file_data.file_name)
             tag = file_data.mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+            html = html.replace('{file_id}', str(id))
+            html = html.replace('{secondbot}', Var.SECOND_BOTUSERNAME)
+                    
     elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
         async with aiofiles.open('Adarsh/template/req.html') as r:
             heading = 'Listen {}'.format(file_data.file_name)
             tag = file_data.mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+            html = html.replace('{file_id}', str(id))
+            html = html.replace('{secondbot}', Var.SECOND_BOTUSERNAME)
+                    
     else:
         async with aiofiles.open('Adarsh/template/dl.html') as r:
             async with aiohttp.ClientSession() as s:
@@ -34,6 +40,10 @@ async def render_page(id, secure_hash):
                     heading = 'Download {}'.format(file_data.file_name)
                     file_size = humanbytes(int(u.headers.get('Content-Length')))
                     html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+                    html = html.replace('{file_id}', str(id))
+                    html = html.replace('{secondbot}', Var.SECOND_BOTUSERNAME)
+                    
+
     return html
 
 async def media_watch(id):
@@ -47,6 +57,8 @@ async def media_watch(id):
             heading = 'Watch - {}'.format(file_name)
             tag = file_data.mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+            html = html.replace('{file_id}', str(id))
+            html = html.replace('{secondbot}', Var.SECOND_BOTUSERNAME)
     else:
         html = '<h1>This is not streamable file</h1>'
     return html
