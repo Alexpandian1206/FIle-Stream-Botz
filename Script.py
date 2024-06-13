946280b044e37831c00023b6e9aacd68c79de815
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-06-14 00:02:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,10 +1,10 @@
 class script(object):
     CAPTION = """
-<b>══════════════════
-📁 𝑭𝒊𝒍𝒆 𝑵𝒂𝒎𝒆 : <blockquote> {filename} </blockquote>\n═══════════════════\n
-🔘 𝑭𝒊𝒍𝒆 𝑺𝒊𝒛𝒆 : {file_size}
+File Name: <blockquote> {filename} </blockquote>
+
+💾 Sɪᴢᴇ: {filesize}
 
 ╔═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
 💥 𝙅𝙊𝙄𝙉 :- <a href='https://t.me/Aklinksz'>AKLINKSZ</a>
 💥 𝙅𝙊𝙄𝙉 :- <a href='https://t.me/Aklinksz'>AKLINKSZ</a>
-╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝</b>"""
+╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝"""
