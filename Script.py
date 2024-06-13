bcde0b4aa0ae8cef294363e18c7a13ca7128a059
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-06-13 21:07:25 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,10 @@
+class script(object):
+    CAPTION = """
+<b>══════════════════
+📁 𝑭𝒊𝒍𝒆 𝑵𝒂𝒎𝒆 : {file_name}\n═══════════════════\n
+🔘 𝑭𝒊𝒍𝒆 𝑺𝒊𝒛𝒆 : {file_size}
+
+╔══════════════════╗
+       <a href=https://t.me/Aklinksz>ᴊᴏɪɴ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a>
+       <a href=https://t.me/Aklinksz>ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ​​</a>
+╚══════════════════╝</b>"""
