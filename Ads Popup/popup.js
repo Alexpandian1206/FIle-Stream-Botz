cbf5898a3adbd3d84f5a0a65e595355332906968
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-08-18 19:45:05 +0530

    Delete Ads Popup directory

--- Ads Popup/popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const popup = document.getElementById("ad-popup");
-
-    // Set a timer to close the popup after 5 seconds
-    setTimeout(function() {
-        popup.classList.add("hide");
-    }, 5000);  // 5000ms = 5 seconds
-});
